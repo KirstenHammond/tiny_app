69f65fdad099330718512e06b49458bcb9da0a6f
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-28 15:52:56 -0800

    added new route for urls/:id for each shortened url navigated to from the main page

--- express_server.js
+++ express_server.js
@@ -9,13 +9,19 @@ app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
+  "9sm5xK": "http://www.google.com",
+  "5we8aY": "http://www.guardian.co.uk"
 };
 
 app.get("/urls", (req, res) => {
   const templateVars = {urls : urlDatabase};
   res.render("urls_index", templateVars);
 })
+
+app.get("/urls/:id", (req, res) => {
+  const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
+  res.render("urls_show", templateVars);
+});
 
 app.get("/", (req, res) => {
   res.send("Hello!");
