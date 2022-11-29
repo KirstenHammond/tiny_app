0ac4e2a6e8f6390c40dfcf87cb68cae02e1c85db
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-29 13:12:52 -0800

    add app.get for 404 status code

--- express_server.js
+++ express_server.js
@@ -19,7 +19,12 @@ const randomString = () => {
 //console.log(randomString());
 
 app.set("view engine", "ejs"); //setting the EJS view engine to recognise the views folder
-app.use(express.urlencoded({ extended: true })); //converting the server response body from buffer to encoded readable language
+app.use(express.urlencoded({ extended: true })); //MIDDLEWARE converting the server response body from buffer to encoded readable language
+app.use((req, res, next) => { //MIDDLEWARE
+  console.log(`${req.method} ${req.url}`); //for every request, do this
+  next(); //starts a chain of callbacks thatll run with every request
+})
+
 
 
 const urlDatabase = { //presumably this will be refactored from a database and not hard coded
@@ -29,14 +34,10 @@ const urlDatabase = { //presumably this will be refactored from a database and n
 };
 
 app.get("/urls", (req, res) => { //main table housing historical conversions when signed in
+  console.log(urlDatabase);//logs to server not client side so we can track movements on database
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 })
-
-app.get("/urls/new", (req, res) => {// creating a new submission. It has a linked POST request.
-  res.render("urls_new");
-});
-
 
 app.post("/urls", (req, res) => { //POST request for when user submits long url. 
   const id = randomString();//short url generator for each post
@@ -45,18 +46,42 @@ app.post("/urls", (req, res) => { //POST request for when user submits long url.
   res.redirect(`/urls/${id}`);// eg urls_show = redirect to a page displaying long url and shortened URL as a hyperlink (see below)
 })
 
+
+app.get("/urls/new", (req, res) => {// creating a new submission. It has a linked POST request.
+  res.render("urls_new");
+});
+
+
+
 app.get("/u/:id", (req, res) => {
-    const longURL = urlDatabase[req.params.id];//getting the longURL from the post above as it has been saved to the database
-    //console.log(longURL); testing
-    return res.redirect(longURL);//when u/shortURL is visited, it redirects to the long URL
+  const longURL = urlDatabase[req.params.id];//getting the longURL from the post above as it has been saved to the database
+  //console.log(longURL); testing
+  return res.redirect(longURL);//when u/shortURL is visited, it redirects to the long URL
 });
 
 app.get("/urls/:id", (req, res) => { //URL specific page detailing the long and short URL and rendering urls_show
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
 });
-//console.log(urlDatabase);//logs to server not client side
+
+app.get("/*" , (req, res) => {
+  res.render('404'); // // where 404 is a file path with ejs view.Currently only working on /
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}!`);
 });
+
+
+/* 
+
+   <!--<a href="/singles/delete/<%= name %> Delete </a>-->
+   <!--<a href="/singles/delete/<%= name %> Edit </a>-->
+At the bottom so every other route gets filtered through before triggering this render
+console log database to see what the database looks like througout requests
+app.get('singles/delete/:name', (req, res) => {
+  const name = req.params.name;
+  delete singles[name];
+  res.redirect('/');
+}) */
