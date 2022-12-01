c5e0f721b38797b01d4ac767b10bfb173459ec1e
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-11-30 16:51:30 -0800

    created helper function to get user object from email. added conditionals in login and register to check clashes against existing database.

--- express_server.js
+++ express_server.js
@@ -1,11 +1,19 @@
+//Tiny App
 
-//Using Express to create a server on port 8080
+//Create a website and server that reduces long urls to short urls. Users must be logged in, after which they will gain acess to their database of shortened URLs
+
+//---------------------------------------------------------------------------------------------------------------------
+//SETUP
 
 const express = require("express");
 const cookieParser = require('cookie-parser'); //installed npm install cookie-parser
 const app = express();
-app.use(cookieParser());//calling the cookie parser function within express()
+app.set("view engine", "ejs"); //setting the EJS view engine to recognise the views folder
 const PORT = 8080; // default port 8080
+
+
+//------------------------------------------------------------------------------------------------------------
+//GLOBAL FUNCTIONS
 
 //Function to create short URL
 const randomString = () => {
@@ -19,9 +27,23 @@ const randomString = () => {
   return result;
 }
 
-//console.log(randomString());//testing
+const getUserByEmail = (emailProvided) => {
+  let userObjectByEmail = {};
+  for (let existingIDs in users) {
+    //console.log("users[existingIDs].email", users[existingIDs].email);
+    if (users[existingIDs].email === emailProvided) {
+      //console.log("users[existingIDs]", users[existingIDs]);
+      userObjectByEmail = users[existingIDs];
+    } else {
+      userObjectByEmail = null;
+    }
+  }
+  return userObjectByEmail;
+}
+//-----------------------------------------------------------------------------------------------------------------------------
+//MIDDLEWARE
 
-app.set("view engine", "ejs"); //setting the EJS view engine to recognise the views folder
+app.use(cookieParser());//calling the cookie parser function within express()
 app.use(express.urlencoded({ extended: true })); //MIDDLEWARE converting the server response body from buffer to encoded readable language
 app.use((req, res, next) => { //MIDDLEWARE
   console.log(`reqmethod= ${req.method}  requrl= ${req.url}`); //for every request, do this
@@ -29,99 +51,219 @@ app.use((req, res, next) => { //MIDDLEWARE
 })
 
 
+//---------------------------------------------------------------------------------------------------------------------
+//DATABASES
 
+//URL Database
 const urlDatabase = { //presumably this will be refactored from a database and not hard coded
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com",
   "5we8aY": "http://www.guardian.co.uk"
 };
 
+
+//User Database
+const users = {
+  userRandomID: {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur",
+  },
+  user2RandomID: {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk",
+  },
+};
+
+//------------------------------------------------------------------------------------------------------------------
+//ROUTES
+
+
+//Main Page
 app.get("/urls", (req, res) => { //main table housing historical conversions when signed in
-  //console.log(urlDatabase);//logs to server not client side so we can track movements on database
-  //console.log("req.cookies[username]", req.cookies["username"]);///testing
-  const templateVars = { 
+  //console.log(users);//logs to server not client side so we can track movements on databases
+  const user_id = req.cookies["user_id"];
+  let userObject = {};
+  for (let existingID in users) {
+    if (existingID === user_id) {
+      userObject = users[existingID];
+    }
+  }
+  const templateVars = {
+    userObject,  //adding access to the cookie user_id in the header template
     urls: urlDatabase,
-    username : req.cookies["username"],  //adding access to the cookie username in the header template
     dateCreated: new Date().toLocaleDateString()///stretch project
   };
   res.render("urls_index", templateVars);
 })
 
-app.post("/urls", (req, res) => { //POST request for when user submits long url from /urls/new. 
-  const id = randomString();//short url generator for each post
-  const newEntry = `http://${req.body.longURL}`;//making the format readable when redirecting
-  urlDatabase[id] = newEntry; //adding the new short url and provided long url to database
-  res.redirect(`/urls/${id}`);// eg urls_show = redirect to a page displaying long url and shortened URL as a hyperlink (see below)
-})
 
-app.post("/login", (req, res) => { //event handler for clicking login button with submitted username
-  //console.log("req.body=", req.body.username); //testing. the string that was passed in
-  res.cookie("username", `${req.body.username}`, {encode: String}); //setting a cookie named username to the value of the encoded string passed in
-  res.redirect("/urls"); //redirecting to the home page. Header has conditional to check if a username cookie is truthy
-})
+//Login
 
-app.post("/logout", (req, res) => {//when username cookie is truthy, a form appears notifying the user of their username and providing an option to sign out
-  //console.log("logout", req.cookies.username);//checking
-  res.clearCookie("username");//upon clicking logout, the cookie called username is cleared
-  res.redirect("/urls");//redirected to home page, now condition is falsey so login option appears
+//GET Login
+app.get("/login", (req, res) => {
+  const user_id = req.cookies["user_id"];
+  let userObject = {};
+  for (let existingID in users) {
+    if (existingID === user_id) {
+      userObject = users[existingID];
+    }
+  }
+  const templateVars = {
+    userObject //adding access to the cookie user_id in the header template
+  }
+  res.render("login", templateVars);
 });
 
 
-app.get("/urls/new", (req, res) => {// creating a new submission. It has a linked POST request.
+//POST login
+app.post("/login", (req, res) => {
+  console.log("req.body", req.body);
+  
+  //const loginId = randomString(); //using the same shortURL helper function
+  const loginEmail = req.body.email;
+  const loginPassword = req.body.password;
+
+  let userObjectLogin = getUserByEmail(loginEmail);
+  console.log("userobjectlogin", userObjectLogin);
+
+  if (userObjectLogin && (userObjectLogin.password === loginPassword)) {
+    res.cookie("user_id", userObjectLogin.id, { encode: String });
+    res.redirect('urls');
+  } else {
+    res.sendStatus(403)
+  }
+});
+
+//Logout
+app.post("/logout", (req, res) => {//when user_id cookie is truthy
+  //console.log("logout", req.cookies.user_id);//checking
+  res.clearCookie("user_id");//upon clicking logout, the cookie called user_id is cleared
+  res.redirect("/login");//redirected to home page, now condition is falsey so login option appears
+});
+
+
+//Register. Adds to users database
+app.get("/register", (req, res) => { //rendering for /register page where users submit their email and password to create a new user
+  const user_id = req.cookies["user_id"];
+  let userObject = {};
+  for (let existingID in users) {
+    if (existingID === user_id) {
+      userObject = users[existingID];
+    }
+  }
   const templateVars = {
-    username: req.cookies.username //adding access to the cookie username in the header template
+    userObject //adding access to the cookie user_id in the header template
+  }
+  res.render("register", templateVars);
+});
+
+app.post("/register", (req, res) => {//event handler for submissions of email and password- adds them to the users database
+  const id = randomString(); //using the same shortURL helper function
+  const email = req.body.email;
+  const password = req.body.password;
+
+  let registerUserObject = getUserByEmail(email);//returning an object that matches the ID, if it exists
+  if (registerUserObject === null && (password !== "" || email !== "")) {//if the user isnt already in the database and on the condition that the fields arent empty
+    users[id] = { id, email, password }; //add the user to the user database
+    res.cookie("user_id", `${id}`, { encode: String });
+    res.redirect('/urls');
+  } else {
+    res.sendStatus(400);
+  }
+  //console.log("registerUserObject", registerUserObject); //checking the value of the id object, if it doesnt exists = null
+  //console.log("users database reg", users); //checking they havent been added on twice
+});
+
+
+//New URL shortener
+app.get("/urls/new", (req, res) => {// creating a new submission. It has a linked POST request.
+  const user_id = req.cookies["user_id"];
+  let userObject = {};
+  for (let existingID in users) {
+    if (existingID === user_id) {
+      userObject = users[existingID];
+    }
+  }
+  const templateVars = {
+    userObject //adding access to the cookie user_id in the header template
   }
   res.render("urls_new", templateVars);
 });
 
+app.post("/urls", (req, res) => { //POST request for when user submits long url from /urls/new. 
+  const shortURL = randomString();//short url generator for each post
+  const newEntry = `http://${req.body.longURL}`;//making the format readable when redirecting
+  urlDatabase[shortURL] = newEntry; //adding the new short url and provided long url to database
+  res.redirect(`/urls/${shortURL}`);// eg urls_show = redirect to a page displaying long url and shortened URL as a hyperlink (see below)
+})
 
-app.get("/u/:id", (req, res) => {
-  const id = req.params.id;
-  const longURL = urlDatabase[req.params.id];//getting the longURL from the post above as it has been saved to the database
-  //console.log(longURL); testing
-  if (urlDatabase.hasOwnProperty(id)) {//checking if id is truthy in urlDatabase.
-  return res.redirect(longURL);//when u/shortURL is visited, it redirects to the long URL
+//Redirect to long URL
+app.get("/u/:shortURL", (req, res) => {
+  const shortURL = req.params.shortURL;
+  const longURL = urlDatabase[shortURL];//getting the longURL from the post above as it has been saved to the database
+  if (urlDatabase.hasOwnProperty(shortURL)) {//checking if shortURL is truthy in urlDatabase.
+    return res.redirect(longURL);//when u/shortURL is visited, it redirects to the long URL
   }
   else {
     res.render('404');
   }
 });
 
-app.get("/urls/:id", (req, res) => { //URL specific page detailing the long and short URL and rendering urls_show
-  const templateVars = { 
-    id: req.params.id, 
-    longURL: urlDatabase[req.params.id],
-    username: req.cookies.username, //adding access to the cookie username in the header template
+
+//Edit/show each short URL
+app.get("/urls/:shortURL", (req, res) => { //URL specific page detailing the long and short URL and rendering urls_show
+
+  const user_id = req.cookies["user_id"];
+  let userObject = {};
+  for (let existingID in users) {
+    if (existingID === user_id) {
+      userObject = users[existingID];
+    }
+  }
+  const templateVars = {
+    userObject, //adding access to the cookie user_id in the header template
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[req.params.shortURL],
     dateCreated: new Date().toLocaleDateString()///stretch projet
   };
-  if(urlDatabase.hasOwnProperty(templateVars.id)) { //cheking that the id that has been requested exists in the database
-  res.render("urls_show", templateVars); //if it does then proceed with rendering urls/show
+  if (urlDatabase.hasOwnProperty(templateVars.shortURL)) { //cheking that the id that has been requested exists in the database
+    res.render("urls_show", templateVars); //if it does then proceed with rendering urls/show
   }
   else {
     res.render('404');
   }
 });
 
-app.post('/urls/:id/delete', (req, res) => { //the POST handler for when the delete button is clicked next to the long URL in /urls
-  const id = req.params.id;
-  delete urlDatabase[id]; //delete keyword to remove item from database, accessed by the key
-  res.redirect('/urls');
-});
-
-app.post('/urls/:id', (req, res) => {//POST handler for when the edit button is clicked in urls_show. It takes the longURL submitted and updated the database
-  //console.log(req.body);//testing
+app.post('/urls/:shortURL', (req, res) => {//POST handler for when the edit button is clicked in urls_show. It takes the longURL submitted and updated the database
   const editedURL = `http://${req.body.longURL}`;
-  const id = req.params.id;
-  urlDatabase[id] = editedURL;
+  const shortURL = req.params.shortURL;
+  urlDatabase[shortURL] = editedURL;//updating the database with the new submission
   res.redirect('/urls');
 });
 
+
+//Delete
+app.post('/urls/:shortURL/delete', (req, res) => { //the POST handler for when the delete button is clicked next to the long URL in /urls
+  const shortURL = req.params.shortURL;
+  delete urlDatabase[shortURL]; //delete keyword to remove item from database, accessed by the key
+  res.redirect('/urls');
+});
+
+
+
+
+//------------------------------------------------------------------------------------------------------------------
+//ERROR HANDLING
 //At the bottom so every other route gets filtered through before triggering this render
 app.get("/*", (req, res) => {
   res.render('404'); // // where 404 is a file path with ejs view.Currently only working on /
 });
 
 
+//--------------------------------------------------------------------------------------------------------------------------
+//LISTEN
 app.listen(PORT, () => {
   console.log(`Server running on ${PORT}!`);
 });
