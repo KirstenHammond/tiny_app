d3debb361cc93dac175aac484efbe98aea417a75
Kirsten Hammond <113560819+KirstenHammond@users.noreply.github.com>
2022-12-02 12:05:35 -0800

    refactored getUserObject function

--- helperFunctions.js
+++ helperFunctions.js
@@ -1,6 +1,6 @@
 // Module for helper functions- imported into express_server.js.
 // All functions global and not dependent on static databases.
- 
+
 // Takes in an email and a database and returns an object eg {id: "gdg4B3", email : k@k.com, password: [encrypted]}.
 const getUserByEmail = (emailProvided, database) => {
   for (let existingID in database) {
@@ -11,11 +11,23 @@ const getUserByEmail = (emailProvided, database) => {
   return null;
 };
 
-//Function to create unique TinyURL and user ID.
+
+// Returns userObject when given user_id and userDatabase
+const getUserObject = (userID, database) => {
+  for (let existingID in database) {
+    if (existingID === userID) {
+      return database[existingID];
+    }
+  }
+};
+
+
+
+// Function to create unique TinyURL and user ID.
 const randomString = () => {
   let result = "";
   const options = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-  options.split(',').map(function() {
+  options.split(',').map(function () {
     for (let i = 0; i < 6; i++) { // Setting loop to stop at 6 iterations, ie 6 characters long.
       result += options.charAt(Math.floor(Math.random() * options.length)); // Using Math.random to select a random number and then add that corresponding character to the string.
     }
@@ -24,7 +36,7 @@ const randomString = () => {
 };
 
 
-//For determing logged in status.
+// For determing logged in status.
 const isLoggedIn = cookie => {
   return cookie ? true : false;
 };
@@ -34,7 +46,7 @@ const isLoggedIn = cookie => {
 const urlsForUser = (user_id, database) => {
   let filteredURLS = {};
   for (let tinyURL in database) {
-    if (database[tinyURL].userID === user_id) { 
+    if (database[tinyURL].userID === user_id) {
       filteredURLS[tinyURL] = database[tinyURL]; // Populating an object of objects containing the users URL details
     }
   }
@@ -50,4 +62,4 @@ const count = () => {
 }
  */
 
-module.exports = { getUserByEmail, randomString, isLoggedIn, urlsForUser };
+module.exports = { getUserByEmail, getUserObject, randomString, isLoggedIn, urlsForUser };
